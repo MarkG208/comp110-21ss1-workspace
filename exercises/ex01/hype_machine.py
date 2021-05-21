71a18adbc8d749c9709cfedb6afa1bcdd85fb93e
Mark Garcia <markag@email.unc.edu>
2021-05-21 15:04:31 -0400

    "Completion of Exercise 01"

--- exercises/ex01/hype_machine.py
+++ exercises/ex01/hype_machine.py
@@ -1,5 +1,10 @@
-"""DESCRIPTION OF PROGRAM HERE"""
+"""The Hype Machine exercise is now completed."""
 
-__author__: str = "YOUR PID HERE"
+__author__: str = "730399808"
 
-# Your solution starts here...
+name: str = input("What is your name? ") 
+print("You entered: ")
+print(name)
+print(name + ", you are an amazing person, keep it up!")
+print("Don't stop you're almost there " + name + ".")
+print("You want to know something " + name + "....you are more than capable of doing this.")
--- exercises/ex01/numeric_operators.py
+++ exercises/ex01/numeric_operators.py
@@ -1,5 +1,18 @@
-"""DESCRIPTION OF PROGRAM HERE"""
+"""The numeric operator code calculates the entered values with multiple expressions."""
 
-__author__: str = "YOUR PID HERE"
+__author__: str = "730399808"
 
-# Your solution starts here...
+Left_hand_side: str = input("What is the left-hand side variable? ")
+Right_hand_side: str = input("What is the right-hand variable? ")
+Left_hand_side_int: int = int(Left_hand_side)
+Right_hand_side_int: int = int(Right_hand_side)
+print("Left-hand side: " + Left_hand_side)
+print("Right-hand side: " + Right_hand_side)
+Exponentiation : int = Left_hand_side_int ** Right_hand_side_int
+print(str(Left_hand_side) + " ** " + str(Right_hand_side) + " is " + str(Exponentiation))
+Division : int = Left_hand_side_int / Right_hand_side_int
+print(str(Left_hand_side) + " / " + str(Right_hand_side) + " is " + str(Division))
+Integer_Divison : int = Left_hand_side_int // Right_hand_side_int
+print(str(Left_hand_side) + " // " + str(Right_hand_side) + " is " + str(Integer_Divison))
+Remainder : int = Left_hand_side_int % Right_hand_side_int
+print(str(Left_hand_side) + " % " + str(Right_hand_side) + " is " + str(Remainder))
