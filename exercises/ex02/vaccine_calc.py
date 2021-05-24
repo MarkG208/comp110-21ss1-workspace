184ef5a430f6f0117d5d7025b3624dacd079e433
Mark Garcia <markag@email.unc.edu>
2021-05-24 17:14:36 -0400

    "Updated Completion of Vaccine Calculator"

--- exercises/ex02/vaccine_calc.py
+++ exercises/ex02/vaccine_calc.py
@@ -20,13 +20,13 @@ from datetime import timedelta
 Population: str = input("What is the population count? ")
 Doses_Administered: str = input("What is the number of doses adminstered? ")
 Doses_Per_Day: str = input("What is the number of doses per day? ")
-Target_Percent_Vaccinated: str = input("What is the target percent vaccinated? ")
+Target_Percent: str = input("What is the target percent vaccinated? ")
 Population_int: int = int(Population)
 Doses_Administered_int: int = int(Doses_Administered)
 Doses_Per_Day_int: int = int(Doses_Per_Day)
-Target_Percent_Vaccinated_int: float = int(Target_Percent_Vaccinated) / 100
+Target_Percent_int: float = int(Target_Percent) / 100
 current_percentage: float = (Doses_Administered_int) / (Population_int * 2)
-Percentage_to_go: float = Target_Percent_Vaccinated_int - current_percentage
+Percentage_to_go: float = Target_Percent_int - current_percentage
 Vaccines_needed: int = round(Percentage_to_go * Population_int * 2)
 Days_to_go: int = round(Vaccines_needed / Doses_Per_Day_int)
 Duration: timedelta = timedelta(Days_to_go)
@@ -34,6 +34,6 @@ Final_date: datetime = datetime.today() + Duration
 print("Population: " + Population)
 print("Doses administered: " + Doses_Administered)
 print("Doses per day: " + Doses_Per_Day)
-print("Target percent vaccinated: " + Target_Percent_Vaccinated)
+print("Target percent vaccinated: " + Target_Percent)
 today: datetime = datetime.today()
-print("We will reach "  +  str(Target_Percent_Vaccinated) + "% " "vaccination" + " in " + str(Days_to_go) + " days" + ", " + "which falls on" + Final_date.strftime(" %B %d, %Y."))
+print("We will reach " + str(Target_Percent) + "% vaccination in " + str(Days_to_go) + " days, which falls on" + Final_date.strftime(" %B %d, %Y."))
