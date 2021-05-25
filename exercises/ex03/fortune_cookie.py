677929ccd96e84b358a0f8ecb2bb03b12549df8e
Mark Garcia <markag@email.unc.edu>
2021-05-25 18:37:03 -0400

    "Completion of Exercise 03"

--- exercises/ex03/fortune_cookie.py
+++ exercises/ex03/fortune_cookie.py
@@ -1,6 +1,6 @@
 """Program that outputs one of at least four random, good fortunes."""
 
-__author__ = "ENTER YOUR 9-DIGIT PID HERE"
+__author__ = "730399808"
 
 # The randint function is imported from the random library so that
 # you are able to generate integers at random.
@@ -11,5 +11,18 @@ __author__ = "ENTER YOUR 9-DIGIT PID HERE"
 # It will evaluate to an int value >= 1 and <= 100. 
 from random import randint
 
+print("Your fortune cookie says...")
+Fortune_Cookie: int = int(randint(1, 3))
 
-# Begin your solution here...
+if Fortune_Cookie == 1:
+    print("The road to success and glory is intimidating yet fulfilling at the end.")
+else:
+    if Fortune_Cookie == 2:
+        print("Right in front of your eyes is the love of your life.")
+    else:
+        if Fortune_Cookie == 3:
+            print("Waiting for you later, is a pleasant surprise.")
+        else:
+            print("Tomorrow will be a better day. And if not, there's another tomorrow to experience")
+
+print("Now, go spread positive vibes!")
--- exercises/ex03/tar_heels.py
+++ exercises/ex03/tar_heels.py
@@ -1,6 +1,20 @@
 """An exercise in remainders and boolean logic."""
 
-__author__ = "ENTER YOUR 9-DIGIT PID HERE"
+__author__ = "730399808"
 
 
-# Begin your solution here...
+Integer: int = int(input("Enter an int: "))
+
+if Integer % 2 != 0 and Integer % 7 != 0:
+    print("CAROLINA")
+if Integer % 2 == 0 and Integer % 7 == 0:
+    print("TAR HEELS")
+else:
+    if Integer % 2 == 0:
+        print("TAR")
+    if Integer % 7 == 0:
+        print("HEELS")
+
+
+
+ 
