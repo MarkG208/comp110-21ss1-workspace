7c0aa2ce910296f9ebbd525de59dd28beeb29df5
Mark Garcia <markag@email.unc.edu>
2021-05-26 18:17:40 -0400

    "Finished Lesson 04"

--- exercises/ex04/factorial.py
+++ exercises/ex04/factorial.py
@@ -1,6 +1,14 @@
 """An exercise in computing the factorial of an int."""
 
-__author__ = "ENTER YOUR 9-DIGIT PID HERE"
+__author__ = "730399808"
 
 
-# Begin your solution here...
+Number: int = int(input("Choose a number: "))
+
+Factorial = 1
+while Number  > 0:
+    Factorial = Factorial * Number
+    Number = Number - 1
+
+print("Factorial: " + str(Factorial))
+
--- exercises/ex04/repeat_beat.py
+++ exercises/ex04/repeat_beat.py
@@ -1,6 +1,23 @@
 """Repeating a beat in a loop."""
 
-__author__ = "ENTER YOUR 9-DIGIT PID HERE"
+__author__ = "730399808"
 
 
-# Begin your solution here...
+Beat_Repeated: str = str(input("What beat do you want to repeat? "))
+Number_of_Repeats: int = int(input("How many times do you want to repeat it? "))
+Result: str = ""
+
+Repeat: int = 0
+while Repeat < Number_of_Repeats:
+  Result += Beat_Repeated + " "
+  Repeat += 1
+else:
+  if Repeat <= 0:
+    Result = ("No beats...")
+
+print(Result)
+
+ 
+
+
+
